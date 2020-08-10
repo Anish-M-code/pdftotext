b1adba5fe603164eee3f5beb73a5907471d8d0ee
ANISH M <58029804+Anish-M-code@users.noreply.github.com>
2020-08-10 19:32:07 +0530

    Minor Bug Fix
    Fixed Bug PDFtoTEXT not working when more than one PDF file existed in current working directory.

---
+++ pdf2text.py
@@ -0,0 +1,48 @@
+# PDFtoTEXT
+# Copyright (C) 2018-2021 M.Anish <aneesh25861@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+''' This program converts all PDF files in current working directory to text Files.'''
+
+import os
+
+#Loop to get names of all PDF files in current working Directory.
+for i in os.listdir():
+  if i[-3:].lower()=='pdf':
+     
+     #create an output folder.
+     os.mkdir(i+'_output')
+     
+     #Move PDF file to output folder.
+     os.system('mv '+i+' '+i+'_output')
+     
+     #Change directory to output folder.
+     os.chdir(i+'_output')
+     
+     #Convert PDF file to PNG images using pdftocairo tool in poppler-utils.
+     os.system('pdftocairo '+i+' '+'-png')
+
+  #Loop to get names of all PNG image files in current working directory.
+  for i in os.listdir():
+    if i[-3:].lower()=='png':
+  
+     #Pass the image to Tesseract ocr to recover text from images.
+     os.system('tesseract '+i+' '+i[:-3])
+     
+     #Delete the image generated during Conversion of PDF to Text files.
+     os.remove(i)
+      
+     #Return to current working Directory.
+     os.chdir('..')
